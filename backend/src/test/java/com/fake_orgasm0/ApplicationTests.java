091a06df4da313a0fd9095aa136a5f11460f074e
Santiago C. Saavedra <gabriel.concha.13120697@usalesiana.edu.bo>
2023-08-17 13:02:21 -0400

    chore: repository creation
    This is the first commit of the repository. It has the following
    technologies:
    
    Web
    	- Spring MVC for the communication bridge beetwen the backend and
      	frontend.
    	- Thymeleaf to make the templates.
    
    Backend
    - Gradle as the java package manager.
    - The Backend will ran using java 19.

---
+++ backend/src/test/java/com/fake_orgasm0/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fake_orgasm0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
