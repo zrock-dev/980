4a2f89169baecc270078f86795694a2adfe61baf
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-08-22 14:06:48 -0400

    build: styles components dependency added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,11 @@
 'use client';
+
+import { GeneralButton } from '@/elements/GeneralElements';
+
 const Home = () => {
 	return (
 		<div>
+			<GeneralButton size={'20px'}>nose</GeneralButton>
 			<img src="/plane-main-page.png" alt="plane-980" />
 		</div>
 	);
