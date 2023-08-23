3d5988b491f3fd1a18e2cd9553155581e4ba0f99
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-08-23 17:43:09 -0400

    style: show and hide suggestions when the input is focused

--- frontend/src/components/navbar/Searcher.js
+++ frontend/src/components/navbar/Searcher.js
@@ -4,14 +4,38 @@ import {
 	SearchSuggestions
 } from '@/elements/Navbar';
 import MagnifyingGlass from '@/icons/MagnifyingGlass';
+import { BLUE, GRAYTWO } from '@/styles/colors';
+import { useRef } from 'react';
 
 const Searcher = () => {
+	const searchContainer = useRef(null);
+	const suggestionsContainer = useRef(null);
+
+	const showSuggestions = () => {
+		if (searchContainer.current && suggestionsContainer.current) {
+			searchContainer.current.style.borderColor = BLUE;
+			suggestionsContainer.current.style.display = 'flex';
+		}
+	};
+
+	const hideSuggestions = () => {
+		if (searchContainer.current && suggestionsContainer.current) {
+			searchContainer.current.style.borderColor = GRAYTWO;
+			suggestionsContainer.current.style.display = 'none';
+		}
+	};
+
 	return (
-		<SearcherContainer>
-			<input type="text" placeholder="Search" />
+		<SearcherContainer ref={searchContainer}>
+			<input
+				type="text"
+				placeholder="Search"
+				onFocus={showSuggestions}
+				onBlur={hideSuggestions}
+			/>
 			<button>{<MagnifyingGlass />}</button>
 
-			<SearchSuggestions>
+			<SearchSuggestions ref={suggestionsContainer}>
 				<SearchSuggestion>Luiggy Mamani</SearchSuggestion>
 				<SearchSuggestion>Luiggy Mamani</SearchSuggestion>
 				<SearchSuggestion>Luiggy Mamani</SearchSuggestion>
