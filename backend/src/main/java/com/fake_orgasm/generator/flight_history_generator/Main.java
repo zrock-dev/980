9f25eb178e3e1dfcd9cd14444e7bc21b5b05aac9
victor0villca <victor.villca.9518420@usalesiana.edu.bo>
2023-08-27 17:24:50 -0400

    task: flight history generator implementation with singleton This commit created the flight history generator with destinationAirport, departureAirport and a ticketType

--- backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/Main.java
+++ backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/Main.java
@@ -1,8 +1,29 @@
 package com.fake_orgasm.generator.flight_history_generator;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        AirportLoader airportLoader = AirportLoader.getInstance();
-        airportLoader.printAirports();
+        int numHistoriesToGenerate = 1000000;
+        FlightHistoryGenerator historyGenerator = FlightHistoryGenerator.getInstance();
+        long startTime = System.currentTimeMillis();
+        List<FlightHistory> flightHistories = historyGenerator.generateFlightHistories(numHistoriesToGenerate);
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        long startTime2 = System.currentTimeMillis();
+        int x = 0;
+        for (FlightHistory flightHistory : flightHistories) {
+            x++;
+            System.out.println("Departure Airport: " + flightHistory.getDepartureAirport().getAirportName());
+            System.out.println("Destination Airport: " + flightHistory.getDestinationAirport().getAirportName());
+            System.out.println("Ticket Type: " + flightHistory.getTicketType());
+            System.out.println(x+ " ------------------------------- " + x);
+        }
+        System.out.println("Generated " + flightHistories.size() + " flight histories.");
+        long endTime2 = System.currentTimeMillis();
+        long elapsedTime2 = endTime2 - startTime2;
+
+        System.out.println("Time taken: " + elapsedTime + " milliseconds");
+        System.out.println("Time taken: " + elapsedTime2 + " milliseconds22");
     }
 }
