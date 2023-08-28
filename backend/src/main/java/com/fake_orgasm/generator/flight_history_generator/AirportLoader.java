fe86ad0c927cc770b868b9ca5446271fbe0b70c5
victor0villca <victor.villca.9518420@usalesiana.edu.bo>
2023-08-27 20:11:15 -0400

    fix:checkstyle fix

--- backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/AirportLoader.java
+++ backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/AirportLoader.java
@@ -10,14 +10,22 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class AirportLoader {
+public final class AirportLoader {
     private static AirportLoader instance = null;
     private List<Airport> airports;
 
+    /**
+     * This is the constructor for this class.
+     */
     private AirportLoader() {
         airports = loadAirportsFromJSON();
     }
 
+    /**
+     * This method returns the singleton instance of the AirportLoader.
+     *
+     * @return The instance of the AirportLoader.
+     */
     public static AirportLoader getInstance() {
         if (instance == null) {
             instance = new AirportLoader();
@@ -25,26 +33,43 @@ public class AirportLoader {
         return instance;
     }
 
+    /**
+     * This method returns a list of loaded airports.
+     *
+     * @return The list of loaded airports.
+     */
     public List<Airport> getAirports() {
         return airports;
     }
 
+    /**
+     * This method loads the airports from the json file.
+     *
+     * @return The list of loaded airports.
+     */
     private List<Airport> loadAirportsFromJSON() {
-        List<Airport> airports_local = new ArrayList<>();
+        List<Airport> airportsLocal = new ArrayList<>();
         try {
             JSONArray airportArray = readAirportArrayFromJSON();
 
             for (Object airportObj : airportArray) {
                 JSONObject airportJson = (JSONObject) airportObj;
                 Airport airport = createAirportFromJson(airportJson);
-                airports_local.add(airport);
+                airportsLocal.add(airport);
             }
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
-        return airports_local;
+        return airportsLocal;
     }
 
+    /**
+     * This method reads the airport array from json.
+     *
+     * @return The JSONArray of loaded airports.
+     * @throws IOException
+     * @throws ParseException
+     */
     private JSONArray readAirportArrayFromJSON() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
         InputStream inputStream = AirportLoader.class.getResourceAsStream("/airports.json");
@@ -54,6 +79,12 @@ public class AirportLoader {
         throw new IOException("Failed to read JSON file.");
     }
 
+    /**
+     * This method creates an airport from a json object.
+     *
+     * @param airportJson
+     * @return The new Airport Object
+     */
     private Airport createAirportFromJson(JSONObject airportJson) {
         String airportName = (String) airportJson.get("airportName");
         String country = (String) airportJson.get("country");
@@ -61,6 +92,9 @@ public class AirportLoader {
         return new Airport(airportName, country, state);
     }
 
+    /**
+     * This method prints the loaded airports to the console.
+     */
     public void printAirports() {
         int i = 0;
         for (Airport airport : airports) {
--- backend/src/test/java/com/fake_orgasm/generator/FlightHistoryGeneratorTest.java
+++ backend/src/test/java/com/fake_orgasm/generator/FlightHistoryGeneratorTest.java
@@ -1,73 +1,109 @@
 package com.fake_orgasm.generator;
-import com.fake_orgasm.generator.flight_history_generator.*;
-import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.fake_orgasm.generator.flight_history_generator.Airport;
+import com.fake_orgasm.generator.flight_history_generator.FlightHistory;
+import com.fake_orgasm.generator.flight_history_generator.FlightHistoryGenerator;
+import com.fake_orgasm.generator.flight_history_generator.Priority;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class FlightHistoryGeneratorTest {
     private FlightHistoryGenerator flightHistoryGenerator;
 
+    /**
+     * This method initializes airport loader in all tests.
+     */
     @BeforeEach
     public void setUp() {
         flightHistoryGenerator = FlightHistoryGenerator.getInstance();
     }
-
 
     @Test
     void flightHistoryGeneratorTest() {
         List<FlightHistory> airports = flightHistoryGenerator.generateFlightHistories(10000);
         assertNotNull(airports);
         assertEquals(10000, airports.size());
-
     }
+
     @Test
     void flightHistoryGeneratorByDepartureTest() {
-        List<FlightHistory> airports = flightHistoryGenerator.generateFlightHistoriesByDepartureAirport("El Alto International Airport", 50000);
-        assertEquals(50000, airports.size());
-        assertEquals("El Alto International Airport",airports.get(0).getDepartureAirport().getAirportName());
-        assertEquals("El Alto International Airport",airports.get(10).getDepartureAirport().getAirportName());
-        assertEquals("El Alto International Airport",airports.get(100).getDepartureAirport().getAirportName());
-        assertEquals("El Alto International Airport",airports.get(1000).getDepartureAirport().getAirportName());
-        assertEquals("El Alto International Airport",airports.get(10000).getDepartureAirport().getAirportName());
-        assertEquals("El Alto International Airport",airports.get(49999).getDepartureAirport().getAirportName());
+        String dp = "El Alto International Airport";
+        int n =  50000;
+        var flights = flightHistoryGenerator.generateFlightByDepartureAirport(dp, n);
+        assertEquals(50000, flights.size());
+
+        assertEquals(
+                "El Alto International Airport",
+                flights.get(0).getDepartureAirport().getAirportName());
+        assertEquals(
+                "El Alto International Airport",
+                flights.get(10).getDepartureAirport().getAirportName());
+        assertEquals(
+                "El Alto International Airport",
+                flights.get(100).getDepartureAirport().getAirportName());
+        assertEquals(
+                "El Alto International Airport",
+                flights.get(1000).getDepartureAirport().getAirportName());
+        assertEquals(
+                "El Alto International Airport",
+                flights.get(10000).getDepartureAirport().getAirportName());
+        assertEquals(
+                "El Alto International Airport",
+                flights.get(49999).getDepartureAirport().getAirportName());
     }
 
     @Test
     void flightHistoryGeneratorBytPriorityTest() {
-        List<FlightHistory> airports = flightHistoryGenerator.generateFlightHistoriesByPriority("VIP", 50000);
+        var airports = flightHistoryGenerator.generateFlightHistoriesByPriority("VIP", 50000);
         assertEquals(50000, airports.size());
-        assertEquals("priority vip",airports.get(0).getTicketType().getPriority());
-        assertEquals("priority vip",airports.get(10).getTicketType().getPriority());
-        assertEquals("priority vip",airports.get(100).getTicketType().getPriority());
-        assertEquals("priority vip",airports.get(1000).getTicketType().getPriority());
-        assertEquals("priority vip",airports.get(10000).getTicketType().getPriority());
-        assertEquals("priority vip",airports.get(49999).getTicketType().getPriority());
+        assertEquals("priority vip", airports.get(0).getTicketType().getPriority());
+        assertEquals("priority vip", airports.get(10).getTicketType().getPriority());
+        assertEquals("priority vip", airports.get(100).getTicketType().getPriority());
+        assertEquals("priority vip", airports.get(1000).getTicketType().getPriority());
+        assertEquals("priority vip", airports.get(10000).getTicketType().getPriority());
+        assertEquals("priority vip", airports.get(49999).getTicketType().getPriority());
     }
+
     @Test
     void flightHistoryGeneratorByDestinationTest() {
-        List<FlightHistory> airports = flightHistoryGenerator.generateFlightHistoriesByDestinationAirport("Leonardo da Vinci–Fiumicino Airport", 50000);
-        assertEquals(50000, airports.size());
-        assertEquals("Leonardo da Vinci–Fiumicino Airport",airports.get(0).getDestinationAirport().getAirportName());
-        assertEquals("Leonardo da Vinci–Fiumicino Airport",airports.get(10).getDestinationAirport().getAirportName());
-        assertEquals("Leonardo da Vinci–Fiumicino Airport",airports.get(100).getDestinationAirport().getAirportName());
-        assertEquals("Leonardo da Vinci–Fiumicino Airport",airports.get(1000).getDestinationAirport().getAirportName());
-        assertEquals("Leonardo da Vinci–Fiumicino Airport",airports.get(10000).getDestinationAirport().getAirportName());
-        assertEquals("Leonardo da Vinci–Fiumicino Airport",airports.get(49999).getDestinationAirport().getAirportName());
+        String dt= "Leonardo da Vinci–Fiumicino Airport";
+        int n =  50000;
+        var flights = flightHistoryGenerator.generateFlightByDestination(dt, n);
+        assertEquals(50000, flights.size());
+        assertEquals(
+                "Leonardo da Vinci–Fiumicino Airport",
+                flights.get(0).getDestinationAirport().getAirportName());
+        assertEquals(
+                "Leonardo da Vinci–Fiumicino Airport",
+                flights.get(10).getDestinationAirport().getAirportName());
+        assertEquals(
+                "Leonardo da Vinci–Fiumicino Airport",
+                flights.get(100).getDestinationAirport().getAirportName());
+        assertEquals(
+                "Leonardo da Vinci–Fiumicino Airport",
+                flights.get(1000).getDestinationAirport().getAirportName());
+        assertEquals(
+                "Leonardo da Vinci–Fiumicino Airport",
+                flights.get(10000).getDestinationAirport().getAirportName());
+        assertEquals(
+                "Leonardo da Vinci–Fiumicino Airport",
+                flights.get(49999).getDestinationAirport().getAirportName());
     }
 
     @Test
     void flightHistoryGeneratorPersonalizedTest() {
-        Airport destination =  new Airport("Real Madrid Airport", "Spain","Madrid");
-        Airport departure =  new Airport("Barcelona Airport", "Spain","Barcelona");
-        List<FlightHistory> airports = flightHistoryGenerator.generatePersonalizedFlightHistories(departure,destination,Priority.NORMAL,50000);
-        assertEquals(50000, airports.size());
+        Airport d = new Airport("Real Madrid Airport", "Spain", "Madrid");
+        Airport dep = new Airport("Barcelona Airport", "Spain", "Barcelona");
+        Priority p = Priority.NORMAL;
+        var flights = flightHistoryGenerator.generateCustomFlights(dep, d, p, 50000);
+        assertEquals(50000, flights.size());
 
-        assertEquals(destination.getAirportName(), airports.get(0).getDestinationAirport().getAirportName());
-        assertEquals(departure.getAirportName(), airports.get(0).getDepartureAirport().getAirportName());
-        assertEquals(Priority.NORMAL, airports.get(0).getTicketType());
-
-
+        assertEquals(d.getAirportName(), flights.get(0).getDestinationAirport().getAirportName());
+        assertEquals(dep.getAirportName(), flights.get(0).getDepartureAirport().getAirportName());
+        assertEquals(Priority.NORMAL, flights.get(0).getTicketType());
     }
 }
