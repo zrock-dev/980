f3a3a91d978b20de098608445e148c6e5d2a5cb1
victor0villca <victor.villca.9518420@usalesiana.edu.bo>
2023-08-27 20:41:33 -0400

    fix:lombok implementation

--- backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/Airport.java
+++ backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/Airport.java
@@ -1,5 +1,10 @@
 package com.fake_orgasm.generator.flight_history_generator;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Airport {
     private String airportName;
     private String country;
@@ -15,60 +20,6 @@ public class Airport {
     public Airport(String airportName, String country, String state) {
         this.airportName = airportName;
         this.country = country;
-        this.state = state;
-    }
-
-    /**
-     * This method returns the name of the airport.
-     *
-     * @return The airport's name.
-     */
-    public String getAirportName() {
-        return airportName;
-    }
-
-    /**
-     * This method sets the name of the airport.
-     *
-     * @param airportName The name to set for the airport.
-     */
-    public void setAirportName(String airportName) {
-        this.airportName = airportName;
-    }
-
-    /**
-     * This method returns the country where the airport is located.
-     *
-     * @return The country of the airport.
-     */
-    public String getCountry() {
-        return country;
-    }
-
-    /**
-     * This method sets the country where the airport is located.
-     *
-     * @param country The country to set for the airport.
-     */
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    /**
-     * This method returns the state where the airport is located.
-     *
-     * @return The state of the airport.
-     */
-    public String getState() {
-        return state;
-    }
-
-    /**
-     * This method sets the state where the airport is located.
-     *
-     * @param state The state to set for the airport.
-     */
-    public void setState(String state) {
         this.state = state;
     }
 }
--- backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/FlightHistory.java
+++ backend/src/main/java/com/fake_orgasm/generator/flight_history_generator/FlightHistory.java
@@ -1,5 +1,10 @@
 package com.fake_orgasm.generator.flight_history_generator;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FlightHistory {
     private Airport departureAirport;
     private Airport destinationAirport;
@@ -16,59 +21,5 @@ public class FlightHistory {
         this.departureAirport = departureAirport;
         this.destinationAirport = destinationAirport;
         this.ticketType = priority;
-    }
-
-    /**
-     * This method retrieves the departure airport for this flight history.
-     *
-     * @return The departure airport.
-     */
-    public Airport getDepartureAirport() {
-        return departureAirport;
-    }
-
-    /**
-     * This method sets the departure airport for this flight history.
-     *
-     * @param departureAirport The departure airport to set.
-     */
-    public void setDepartureAirport(Airport departureAirport) {
-        this.departureAirport = departureAirport;
-    }
-
-    /**
-     * This method retrieves the destination airport for this flight history.
-     *
-     * @return The destination airport.
-     */
-    public Airport getDestinationAirport() {
-        return destinationAirport;
-    }
-
-    /**
-     * This method sets the destination airport for this flight history.
-     *
-     * @param destinationAirport The destination airport to set.
-     */
-    public void setDestinationAirport(Airport destinationAirport) {
-        this.destinationAirport = destinationAirport;
-    }
-
-    /**
-     * This method retrieves the ticket priority for this flight history.
-     *
-     * @return The ticket priority.
-     */
-    public Priority getTicketType() {
-        return ticketType;
-    }
-
-    /**
-     * This method sets the ticket priority for this flight history.
-     *
-     * @param ticketType The ticket priority to set.
-     */
-    public void setTicketType(Priority ticketType) {
-        this.ticketType = ticketType;
     }
 }
