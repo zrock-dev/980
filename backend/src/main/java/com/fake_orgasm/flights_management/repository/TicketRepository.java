4242552f58aa492dbe5ca5a1f4fd1fd26a55e65f
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-09-01 18:51:28 -0400

    feat: ticket repository finished

--- backend/src/main/java/com/fake_orgasm/flights_management/repository/TicketRepository.java
+++ backend/src/main/java/com/fake_orgasm/flights_management/repository/TicketRepository.java
@@ -1,60 +1,116 @@
 package com.fake_orgasm.flights_management.repository;
 
+import com.fake_orgasm.flights_management.models.Category;
 import com.fake_orgasm.flights_management.models.Ticket;
-import com.fake_orgasm.flights_management.repository.requesters.JsonRequester;
-import com.fake_orgasm.flights_management.repository.requesters.SourceJson;
-import com.fake_orgasm.flights_management.services.ITicketManagement;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.io.FileWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class TicketRepository implements ITicketManagement {
+public class TicketRepository {
 
-    private JsonRequester requester;
+    private FlightDatabase database;
 
     public TicketRepository() {
-        requester = new JsonRequester(SourceJson.TICKETS.getSource());
+        database = FlightDatabase.getInstance();
     }
 
-    private JSONObject toJsonObject(Ticket ticket) {
-        JSONObject jsonTicket = new JSONObject();
-        jsonTicket.put("id", ticket.getId());
-        jsonTicket.put("number", ticket.getNumber());
-        jsonTicket.put("priority", ticket.getPriority().getType());
-        jsonTicket.put("userId", ticket.getUserId());
-        jsonTicket.put("flightId", ticket.getFlightId());
-        return jsonTicket;
+    public void createTable() {
+        try {
+            Statement stmt;
+            stmt = database.getConnection().createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS Ticket" +
+                    "(id VARCHAR(250) PRIMARY KEY," +
+                    "arrivalNumber VARCHAR(250)," +
+                    "priority VARCHAR(250)," +
+                    "userId  VARCHAR(250)," +
+                    "flightId VARCHAR(250)," +
+                    "FOREIGN KEY (flightId) REFERENCES Flight(id)" +
+                    ");";
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Override
     public boolean create(Ticket ticket) {
         boolean wasSaved = false;
+        if (database.doesNotExist("Ticket", ticket.getId())) return wasSaved;
         try {
-            JSONArray currentDocuments = requester.getDocuments();
-            currentDocuments.put(toJsonObject(ticket));
-            FileWriter writer = requester.getFileWriter();
-            writer.write(currentDocuments.toString(4));
-            writer.flush();
+            String query = "INSERT INTO Flight (id, arrivalNumber, priority, " +
+                    "userId, flightId) " +
+                    "values (?, ?, ?, ?, ?)";
+            PreparedStatement ps = database.getConnection().prepareStatement(query);
+            ps.setString(1, ticket.getId());
+            ps.setInt(2, ticket.getNumber());
+            ps.setString(3, ticket.getPriority().getType());
+            ps.setInt(4, ticket.getUserId());
+            ps.setString(5, ticket.getFlightId());
+            ps.execute();
+            ps.close();
             wasSaved = true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return wasSaved;
     }
 
-    @Override
     public Ticket search(String id) {
-        return null;
+        String query = "SELECT * FROM Ticket WHERE id = ?";
+        try {
+            PreparedStatement ps = database.getConnection().prepareStatement(query);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                int ticketNumber = rs.getInt("arrivalNumber");
+                String priorityType = rs.getString("priority");
+                int userId = rs.getInt("userId");
+                String flightId = rs.getString("flightId");
+                Category category = Category.getCategory(priorityType);
+                Ticket ticket = new Ticket(id, ticketNumber, category, userId, flightId);
+
+                ps.close();
+                return ticket;
+            } else {
+                ps.close();
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Override
+
     public boolean update(String id, Ticket ticket) {
-        return false;
+        boolean wasUpdated = false;
+        try {
+            if (!database.doesNotExist("Ticket", id)) {
+                String query = "UPDATE Ticket SET arrivalNumber=?, priority=?, " +
+                        "userId=?, flightId=? WHERE id=?";
+                PreparedStatement ps = database.getConnection().prepareStatement(query);
+                ps.setInt(1, ticket.getNumber());
+                ps.setString(2, ticket.getPriority().getType());
+                ps.setInt(3, ticket.getUserId());
+                ps.setString(4, ticket.getFlightId());
+                ps.setString(5, id);
+                int rowsUpdated = ps.executeUpdate();
+                ps.close();
+
+                if (rowsUpdated > 0) {
+                    wasUpdated = true;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return wasUpdated;
     }
 
-    @Override
+
     public boolean remove(String id) {
-        return false;
+        return database.remove("Flight", id);
     }
 }
