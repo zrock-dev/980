0c20cbdf3c4f13dd4b7d555f995f3dc9c3b10476
JCarlosHidalgoSosa <juan.hidalgo.8798478@usalesiana.edu.bo>
2023-09-05 08:45:44 -0400

    gateway re-factored and implementation of banners to each service

--- backend/eureka_server/src/test/java/com/fake_orgasm/EurekaServerApplicationTests.java
+++ backend/eureka_server/src/test/java/com/fake_orgasm/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.discovery_service;
+package com.fake_orgasm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EurekaServerApplicationTests {
 
-    @Test
-    void contextLoads() {}
+	@Test
+	void contextLoads() {
+	}
+
 }
