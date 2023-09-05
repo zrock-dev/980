b6674bd1d748c7b719d1a5e04320d4fadf7e9e2c
JCarlosHidalgoSosa <juan.hidalgo.8798478@usalesiana.edu.bo>
2023-09-05 08:49:02 -0400

    checkstyle implementation in gateway

--- backend/eureka_server/src/main/java/com/fake_orgasm/EurekaServerApplication.java
+++ backend/eureka_server/src/main/java/com/fake_orgasm/EurekaServerApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main Class to set up project application.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
+    /**
+     * Method main to launch app.
+     *
+     * @param args It does not expect any args as parameter.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
