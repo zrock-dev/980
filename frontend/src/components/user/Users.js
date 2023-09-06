a1c76a6befca4fd1860c800a8e1e87f2db6a20f1
victor0villca <victor.villca.9518420@usalesiana.edu.bo>
2023-09-05 20:50:26 -0400

    task: user compenent dinamically developed

--- frontend/src/components/user/Users.js
+++ frontend/src/components/user/Users.js
@@ -1,19 +1,67 @@
 'use client';
 import Link from 'next/link';
 import UserSearchOption from './UserSeach';
+const userList = [
 
-const Users = () => {
+	{
+	  firstName: "Diego",
+	  lastName: "Rodriguez",
+	  year: "31",
+	  category: "PREMIUM",
+	  country: "ARGENTINA",
+	},
+	{
+	  firstName: "Hiroshi",
+	  lastName: "Sato",
+	  year: "28",
+	  category: "NORMAL",
+	  country: "JAPAN",
+	},
+	{
+	  firstName: "Isabel",
+	  lastName: "Fernandez",
+	  year: "22",
+	  category: "PREMIUM",
+	  country: "SPAIN",
+	},
+	{
+	  firstName: "William",
+	  lastName: "Davis",
+	  year: "25",
+	  category: "NORMAL",
+	  country: "UNITED KINGDOM",
+	},
+	{
+	  firstName: "Maria",
+	  lastName: "Gonzalez",
+	  year: "29",
+	  category: "NORMAL",
+	  country: "MEXICO",
+	},
+	{
+	  firstName: "Hans",
+	  lastName: "Müller",
+	  year: "33",
+	  category: "VIP",
+	  country: "GERMANY",
+	},
+  ];
+
+  const Users = () => {
 	return (
-		<div>
-			<UserSearchOption
-			firstName= "Maria Natalia"
-			lastName="Montalvo Mamani" 
-			year ="19"
-			category="NORMAL"
-			country = "BOLIVIA"
-			/>
-		</div>
+	  <div>
+		{userList.map((user, index) => (
+		  <UserSearchOption
+			key={index}
+			firstName={user.firstName}
+			lastName={user.lastName}
+			year={user.year}
+			category={user.category}
+			country={user.country}
+		  />
+		))}
+	  </div>
 	);
-};
+  };
 
 export default Users;
