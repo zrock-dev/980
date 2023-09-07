078c91df7bdcd35def1940a6a353db18f4ccef7b
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-09-07 18:41:44 -0400

    style: user elements to build their component

---
+++ frontend/src/elements/User.js
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+
+export const UserContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    max-width: 85%;
+    margin: auto;
+`;
+
+export const UserInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    margin-bottom: 40px;
+`;
+
+export const UserOptionsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const UserMainInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+`
+
+export const UserName = styled.span`
+    text-transform: uppercase;
+    font-size: 35px;
+    font-weight: 700;
+`;
+
+export const UserInformationContainer = styled.div`
+
+`;
+
+export const FlightHistoryContainer = styled.div`
+
+`
